a3c9eb4353b8e4e3c258c54b2e13a32b67be99ee
Waldi <wfubu@web.de>
2021-09-19 23:46:33 +0200

    Finished 1st step - some things I forgot

---
+++ migrations/Version20210918193806.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210918193806 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, created DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE timelog (id INT AUTO_INCREMENT NOT NULL, project_id INT NOT NULL, start DATETIME NOT NULL, end DATETIME NOT NULL, comment VARCHAR(255) DEFAULT NULL, created DATETIME NOT NULL, updated DATETIME NOT NULL, INDEX IDX_B79470CE166D1F9C (project_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE timelog ADD CONSTRAINT FK_B79470CE166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE timelog DROP FOREIGN KEY FK_B79470CE166D1F9C');
+        $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE timelog');
+    }
+}
